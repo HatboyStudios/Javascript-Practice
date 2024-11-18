2e362714bccec22865396027b8075b1cd16e0c88
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-11-18 07:32:53 -0800

    demo sprite

---
+++ libraries/sprite.js
@@ -0,0 +1,19 @@
+class Sprite {
+    constructor(x,y,w,h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.w, this.h);
+        ctx.stroke();
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x+1, this.y+1, this.w-2, this.h-2);
+    }
+}
+
+let test = new Sprite(20,20,50,50);
+test.draw();
